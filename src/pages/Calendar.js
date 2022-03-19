3c79048adb84fff00d56ebc50c36d20d196bb43b
Djordje Djuric <djordjedjuric05@gmail.com>
2022-03-19 22:41:49 +0100

    style
    style update

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -3,13 +3,23 @@ import styled from './Calendar.module.css';
 import { Container, Col, Row } from 'react-bootstrap'; 
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase-config';
 import { getFirestore } from '@firebase/firestore';
-import { collection, addDoc, getDocs, orderBy, limit, query } from '@firebase/firestore';
+import { collection, addDoc, getDocs, orderBy, limit, query, updateDoc,doc } from '@firebase/firestore';
+
 import { useFirestoreQuery } from '../hooks/firebase-hooks';
 import Day from '../UI/Day';
 
 
 const Calendar = () => {
+
+
+    const [User, setUser] = useState(''); 
+
+    onAuthStateChanged(auth,(currentuser)=>{setUser(currentuser)});
+
 
     const db = getFirestore(); 
     const calendarRef = collection(db,"Mart"); 
@@ -20,30 +30,31 @@ const Calendar = () => {
 
 
 
-    const removeWorker = (id) =>{ 
-        console.log(id); 
+    const removeWorker = async (id) =>{ 
+                                                
+         const docID = doc(db,"Mart",id); 
+         console(docID); 
+         await updateDoc(docID,{Radnik_1: null}); 
+ 
 
-    }
+
+
+        }
 
 
 
 let day =  days.map(data => <Col key={data.id}  className={styled.day} lg={2} > <Day removeWorker={removeWorker} day={data} /> </Col>)
 
+if(User!=null){  
 
   return (
+
 <React.Fragment> 
-
 <div className={styled['calendar-body']} >
-    
 <Container className={styled.days}>
-
 <Row>
-
   {day}
-  
   </Row>
-
- 
 </Container>
     
     
@@ -53,7 +64,13 @@ let day =  days.map(data => <Col key={data.id}  className={styled.day} lg={2} > 
      </div>
 
 </React.Fragment>
-  )
+  )} else {
+      return ( 
+        <div>Please Login</div>
+
+      )
+  }
+
 }
 
 export default Calendar
